462a6c3692e5540bdf542e4587b6e153864e4a44
Ian Bryant <48700036+IXLives@users.noreply.github.com>
2019-04-27 14:12:46 -0400

    objects-arrays.js stretch done!

--- challenges/objects-arrays.js
+++ challenges/objects-arrays.js
@@ -146,8 +146,7 @@ The zoo wants to display both the scientific name and the animal name in front o
 */
 const animalNames = [];
 
-zooAnimals.forEach(function(i){
-  animalNames.push(`Name: ${i.animal_name}, Scientific: ${i.scientific_name}.`)
+zooAnimals.forEach( (i) => {animalNames.push(`Name: ${i.animal_name}, Scientific: ${i.scientific_name}.`)
 });
 
 console.log(animalNames);
@@ -159,9 +158,7 @@ The zoos need a list of all their animal's names (names only, not scientific) co
 */
 
 const lowerCase = []; 
-
-zooAnimals.map(function(i){
-  lowerCase.push(i.animal_name.toLowerCase());
+zooAnimals.map( (i) => {lowerCase.push(i.animal_name.toLowerCase());
 });
 
 
@@ -183,8 +180,7 @@ console.log(largerPopulation);
 The zoos need to know their total animal population across the United States.  Find the total population from all the zoos using the .reduce() method.
 
 */
-const populationTotal = zooAnimals.reduce(function(acc, val){
-  return acc + val.population;
+const populationTotal = zooAnimals.reduce( (acc, val) => {return acc + val.population;
 }, 0);
 console.log(populationTotal);
 
