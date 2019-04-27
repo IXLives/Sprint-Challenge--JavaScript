189f6370abb7b5431f44e2bde99b909e43510e9c
Ian Bryant <48700036+IXLives@users.noreply.github.com>
2019-04-27 13:32:40 -0400

    I think MVP is completed. Mental break, then back to check my work.

--- challenges/classes.js
+++ challenges/classes.js
@@ -15,8 +15,12 @@ class CuboMaker {
         return 2 * (this.length * this.width + this.length * this.height + this.width * this.height);
     }
 }
+
+const cubo = new CuboMaker(4, 5, 5);
+
+
 // Test your volume and surfaceArea methods by uncommenting the logs below:
- console.log(cuboid.volume()); // 100
- console.log(cuboid.surfaceArea()); // 130
+ console.log(cubo.volume()); // 100
+ console.log(cubo.surfaceArea()); // 130
 
 // Stretch Task: Extend the base class CuboidMaker with a sub class called CubeMaker.  Find out the formulas for volume and surface area for cubes and create those methods using the dimension properties from CuboidMaker.  Test your work by logging out your volume and surface area.
